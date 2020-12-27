12c46d8213acc2ffb667c3a6310d4690fd54fc67
JingYang <jingyangsddx@163.com>
2020-12-27 21:42:07 +0800

    fix upload issue

--- app/src/main/java/com/king/app/coolg_kt/model/http/upload/UploadService.kt
+++ app/src/main/java/com/king/app/coolg_kt/model/http/upload/UploadService.kt
@@ -9,7 +9,13 @@ import retrofit2.http.POST
 import retrofit2.http.PartMap
 
 interface UploadService {
+
+    /**
+     * RequestBody是abstract类型的，retrofit在kotlin语言下回报出如下异常：
+     * Parameter type must not include a type variable or wildcard
+     * 需要添加@JvmSuppressWildcards的注解
+     */
     @Multipart
     @POST("uploaddb")
-    fun uploadDb(@PartMap params: Map<String, RequestBody>): Observable<BaseResponse<UploadResponse>>
+    fun uploadDb(@PartMap params: Map<String, @JvmSuppressWildcards RequestBody>): Observable<BaseResponse<UploadResponse>>
 }
